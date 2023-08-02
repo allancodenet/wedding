9f65fa78503c4f8c41e2d0c2504847461182e20b
allancodenet <allancodenet@gmail.com>
2023-08-03 00:56:36 +0300

    added images

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-
+ 
 
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::UnlocksController < Devise::UnlocksController
+  layout "user"
   # GET /resource/unlock/new
   # def new
   #   super
