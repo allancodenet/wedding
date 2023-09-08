22d0599307303793e364c565e3021a53798814e4
allancodenet <allancodenet@gmail.com>
2023-09-09 02:43:21 +0300

    added missing ids for messages

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,7 +5,6 @@ class LikesController < ApplicationController
   before_action :client
 
   def update
-    binding.break
     if @provider.liked_by?(client)
       @provider.unlike(client)
     else
--- app/policies/conversation_policy.rb
+++ app/policies/conversation_policy.rb
@@ -1,23 +1,23 @@
 class ConversationPolicy < ApplicationPolicy
-  # def create?
-  #   show?
-  # end
+  def create?
+    show?
+  end
 
-  # def show?
-  #   involved_in_conversation?
-  # end
+  def show?
+    involved_in_conversation?
+  end
 
-  # private
+  private
 
-  # def involved_in_conversation?
-  #   client_recipient? || provider_recipient?
-  # end
+  def involved_in_conversation?
+    client_recipient? || provider_recipient?
+  end
 
-  # def client_recipient?
-  #   user.client == record.client
-  # end
+  def client_recipient?
+    user.client == record.client
+  end
 
-  # def provider_recipient?
-  #   user.provider == record.provider
-  # end
+  def provider_recipient?
+    user.providers == record.provider
+  end
 end
