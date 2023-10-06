0e91320bed4fb75d85cfd9bd0f7d93d7172e044e
allancodenet <allancodenet@gmail.com>
2023-10-06 14:06:53 +0300

    images resize

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="chat"
 export default class extends Controller {
   connect() {
-    console.log("hello chat");
     this.element.addEventListener("turbo:stream:after-append", (event) => {
       // Scroll to the bottom of the chat window to display the new message
       this.element.scrollTop = this.element.scrollHeight;
