a2ace202702d48e4bc134aefcd101a08580245bb
allancodenet <allancodenet@gmail.com>
2023-10-08 00:56:19 +0300

    minor fixes

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,16 +1,16 @@
 class Client < ApplicationRecord
   belongs_to :user
   validates :name, presence: true
-  # validates :phone_number, presence: true
-  # validate :validate_phone_number_length
+  validates :phone_number, presence: true
+  validate :validate_phone_number_length
   has_many :conversations, dependent: :destroy
   has_many :likes, as: :liker, dependent: :destroy
   has_many :ratings, as: :rater, dependent: :destroy
   has_noticed_notifications
 
   def validate_phone_number_length
-    unless phone_number.to_s.length >= 12
-      errors.add(:phone_number, "Check phone number")
+    unless phone_number.to_s.length >= 9
+      errors.add(:phone_number, "Check phone number length")
     end
   end
 end
